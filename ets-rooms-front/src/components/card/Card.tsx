58be9153e76e54f443ad324233572db4ce73d1bd
bryan_vr8 <91436895+bryanvr8@users.noreply.github.com>
2023-08-25 10:40:34 -0300

    v3_bugs

--- ets-rooms-front/src/components/card/Card.tsx
+++ ets-rooms-front/src/components/card/Card.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 
-export default function Card(){
-    return(
-        <section className="w-5/6 h-1/5 rounded bg-white p-1 flex shadow-md">
-            <div className="w-4 h-full rounded-s-lg bg-black">
-
+export default function Card() {
+  return (
+    <section className="w-4/5 h-64 rounded bg-white p-1 flex shadow-md">
+      <div>
+        {itemData.map((item) => (
+          <div className="w-4 h-full rounded-s-lg bg-black">
+            <div className="classroom-infos">
+              <div className="name-time">
+                <h1>{item.labName}</h1>
+                <p>{item.time}</p>
+              </div>
+              <div className="responsible-name">
+                <p>Responsável: {item.responsible}</p>
+              </div>
+              <div className="classroom-name">
+                <p>{item.classroom}</p>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
+
+const itemData = [
+  {
+    labName: "Lab. 01",
+    time: "07:30 - 11:30",
+    responsible: "Leo Augusto",
+    classroom: "Aula de Java",
+  },
+];
--- ets-rooms-front/src/components/view-card-list/ViewCardList.tsx
+++ ets-rooms-front/src/components/view-card-list/ViewCardList.tsx
@@ -3,16 +3,16 @@ import Card from "../card/Card";
 
 export default function ViewCardList(props) {
   return (
-    <>
-      <div className="w-64 h-14 text-center rounded-t-lg bg-slate-100 sticky top-0">
+    <article>
+      <div className="">
         <h1 className="text-2xl font-light text-black">{props.namePremise}</h1>
       </div>
-      <section className="w-64 h-96 rounded-b-lg flex items-center flex-col gap-5 overflow-y-scroll ">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <section className="w-80 h-96 rounded-b-lg bg-slate-100 flex items-center flex-col gap-5 overflow-y-scroll ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
-    </>
+    </article>
   );
 }
