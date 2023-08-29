9b425ee3fdc29fe3c0b291905e850a9ebf7398c5
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-08-29 19:34:42 +0000

    página de agendamento/home

---
+++ ets-rooms-front/src/pages/home/Agendar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Period from "../../components/period/Period";
+import ViewCardList from "../../components/view-card-list/ViewCardList";
+
+export default function Agendar() {
+  return (
+    <main>
+      AGENDAR (COLOCAR O REPOSITORIO DE CALENDÁRIO)
+    </main>
+  );
+}
--- ets-rooms-front/src/router.tsx
+++ ets-rooms-front/src/router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Agendar from "./pages/home/Agendar";
 import EditEvent from "./pages/home/EditEvent";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Agendar />} />
         <Route path="/editar" element={<EditEvent />} />
       </Routes>
     </BrowserRouter>
