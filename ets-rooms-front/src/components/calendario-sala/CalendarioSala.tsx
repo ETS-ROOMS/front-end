6c79cd4e8b370094e0cef8b50b01ead431b66552
Adhayne Daphine <125472212+Adhayne2501@users.noreply.github.com>
2023-09-20 07:50:00 -0300

    calendario melhorado

---
+++ ets-rooms-front/src/components/calendario-sala/CalendarioSala.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "react-calendar/dist/Calendar.css";
+import Calendar from 'react-calendar'; // Importe os outros componentes necessários
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import EventFormModal from '../event-form-modal/EventFormModal';
+
+export default function CalendarioSala() {
+    const [showForm, setShowForm] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [startTime, setStartTime] = useState("07:30");
+    const [endTime, setEndTime] = useState("17:00");
+    const [eventos, setEventos] = useState([]);
+
+    useEffect(() => {
+        fetchEventos();
+    }, []);
+
+    const fetchEventos = async () => {
+        try {
+            const response = await axios.get(`http://127.0.0.1:8000/evento/?format=json`);
+            setEventos(response.data);
+        } catch (error) {
+            console.error("Erro ao buscar Eventos:", error);
+        }
+    };
+
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        setShowForm(false);
+
+
+        const formData = new FormData(event.target);
+        const eventData = {
+            nome_responsavel: formData.get("NomeResponsavel"),
+            nome_evento: formData.get("eventName"),
+            edv_cliente: formData.get("edv"),
+            email: formData.get("responsible"),
+            descricao: formData.get("description"),
+            data_fim: formData.get("datainicio"),
+            data_inicio: formData.get("datafim"),
+            hora_inicio: formData.get("startTime"),
+            hora_fim: formData.get("endTime"),
+
+        };
+
+        try {
+            await axios.post(`http://127.0.0.1:8000/evento/`, eventData);
+            fetchEventos();
+        } catch (error) {
+            console.error("Erro ao enviar Evento:", error);
+        }
+    };
+
+    const handleDateClick = (value) => {
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0);
+
+        if (value.getDay() === 6 || value.getDay() === 0 || value < currentDate) {
+            return;
+        }
+
+        setSelectedDate(value);
+        setShowForm(true);
+    };
+
+
+    const handleCloseForm = () => {
+        setShowForm(false);
+    };
+
+    return (
+        <div className="p-4 h-screen">
+            <div className="flex items-center space-x-48 ">
+                <div className="">
+                    <h1 className="font-bold">Ca600 - Lab 01</h1>
+                </div>
+            </div>
+            <Calendar
+                className=" rounded-lg shadow-md p-2 h-65"
+                prev2Label={null}
+                next2Label={null}
+                prevLabel={<MdKeyboardArrowLeft className="text-4xl text-gray-500" />}
+                nextLabel={<MdKeyboardArrowRight className="text-4xl text-gray-500" />}
+                onChange={handleDateClick}
+                value={selectedDate}
+                tileDisabled={({ date }) => date.getFullYear() !== 2023}
+            />
+            <EventFormModal
+                showForm={showForm}
+                onClose={handleCloseForm}
+                handleFormSubmit={handleFormSubmit}
+                startTime={startTime}
+                setStartTime={setStartTime}
+                endTime={endTime}
+                setEndTime={setEndTime}
+                selectedDate={selectedDate}
+            />
+        </div>
+    );
+}
